f362487318796ee64ddecb24e863abf03fed84d0
KevinPMoore <moore.kevin.p@gmail.com>
2023-03-19 21:33:20 -0400

    temp page to check render

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import tempPage from "./pages/temp"
 
 function App() {
   return (
     <div className="App">
-
+      <Temp/>
     </div>
   );
 }
---
+++ src/pages/temp.jsx
@@ -0,0 +1,9 @@
+
+
+const tempPage = () => {
+    return <div>
+        <p>Well here we are...</p>
+    </div>
+}
+
+export default tempPage 
